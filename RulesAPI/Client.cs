f4bce15eac67fbbf32256be189f0299bb672da8a
@nuj <30503586+rawatanuj@users.noreply.github.com>
2020-11-11 15:40:04 +0530

    Update Client.cs

--- RulesAPI/Client.cs
+++ RulesAPI/Client.cs
@@ -11,7 +11,7 @@ namespace RulesAPI
         public HttpClient AccountClient()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44369");
+            client.BaseAddress = new Uri("http://40.88.220.221");
             return client;
         }
         
